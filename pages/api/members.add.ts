f08071e517ca2376912890cd2714c0de9823321a
gw.jang <gw.jang@cardoc.kr>
2022-09-20 01:48:44 +0900

    2.2 사용자 추가 API 구현

---
+++ pages/api/members.add.ts
@@ -0,0 +1,26 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import FirebaseAdmin from '@/models/firebase_admin';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { uid, email, displayName, photoURL } = req.body;
+  if (!uid) {
+    return res.status(400).json({ result: false, message: 'uid가 누락되었습니다.' });
+  }
+
+  try {
+    const addResult = await FirebaseAdmin.getInstance()
+      .Firebase.collection('members')
+      .doc(uid)
+      .set({
+        uid,
+        email: email ?? '',
+        displayName: displayName ?? '',
+        photoURL: photoURL ?? '',
+      });
+
+    return res.status(200).json({ result: true, data: addResult });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ result: false });
+  }
+}
