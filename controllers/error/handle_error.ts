9d070f42f3db79bb7a71edf874f4f338a89bf5ee
gw.jang-kraken <gunwoooa@naver.com>
2022-10-25 17:59:33 +0900

    2.6 서버사이드 공통 에러 헨들링

---
+++ controllers/error/handle_error.ts
@@ -0,0 +1,17 @@
+import { NextApiResponse } from 'next';
+import CustomServerError from './custom_server_error';
+
+const handleError = (err: unknown, res: NextApiResponse) => {
+  let unknownErr = err;
+  if (!(err instanceof CustomServerError)) {
+    unknownErr = new CustomServerError({ statusCode: 499, message: 'unknown 에러' });
+  }
+
+  const customError = unknownErr as CustomServerError;
+  res
+    .status(customError.statusCode)
+    .setHeader('location', customError.location ?? '')
+    .send(customError.serializeErrors()); // 에러 응답에 body를 전달
+};
+
+export default handleError;
--- pages/api/members.add.ts
+++ pages/api/members.add.ts
@@ -1,16 +1,16 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import MemberCtrl from '@/controllers/member.ctrl';
+import handleError from '@/controllers/error/handle_error';
+import checkSupportMethod from '@/controllers/error/check_support_method';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { method } = req;
   const supportMethod = ['POST'];
   try {
-    if (supportMethod.indexOf(method!) === -1) {
-      // TODO : error 반환
-    }
+    checkSupportMethod(supportMethod, method);
     await MemberCtrl.add(req, res);
   } catch (err) {
-    console.error(err);
-    // TODO : error 처리
+    // error 처리
+    handleError(err, res);
   }
 }
