a0923884987acec82f9a37c881915b1f523e6ba8
gw.jang-kraken <gunwoooa@naver.com>
2022-11-09 21:55:17 +0900

    3.3 질문 입력 컴포넌트 수정

--- pages/[screenName]/index.tsx
+++ pages/[screenName]/index.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable no-alert */
 import { NextPage } from 'next';
-import React, { useState } from 'react';
-import { Avatar, Box, Button, Flex, Text, Textarea } from '@chakra-ui/react';
+import React, { ChangeEvent, EventHandler, useCallback, useState } from 'react';
+import { Avatar, Box, Button, Flex, FormControl, FormLabel, Switch, Text, Textarea } from '@chakra-ui/react';
 import ResizeTextArea from 'react-textarea-autosize';
 import ServiceLayout from '@/components/ServiceLayout';
 import { InAuthUser } from '@/models/in_auth_user';
+import { useAuth } from '@/contexts/AuthUser.context';
 
 const USER_INFO: InAuthUser = {
   uid: 'test',
@@ -12,8 +14,30 @@ const USER_INFO: InAuthUser = {
   photoURL: 'https://lh3.googleusercontent.com/a/ALm5wu1s26VqzFeHv0hdABFJmyqqjA5PBTsTkbGsszf8rA=s96-c',
 };
 
+const ANONYMOUS_IMAGE_URL = '/anonymous.png';
+const ANONYMOUS_SWITCH_ID = 'anonymous';
+
 const UserHomePage: NextPage = function () {
+  const { authUser } = useAuth();
   const [message, setMessage] = useState('');
+  const [isAnonymous, setIsAnonymous] = useState(true);
+
+  const handleChangeTextarea = useCallback((e: ChangeEvent<HTMLTextAreaElement>) => {
+    const { value } = e.currentTarget;
+    const lineCount = value.match(/[^\n]*\n[^\n]*/gi)?.length ?? 0;
+    if (lineCount >= 7) {
+      return;
+    }
+    setMessage(value);
+  }, []);
+
+  const handleChangeSwitch = useCallback(() => {
+    if (authUser === null) {
+      alert('로그인이 필요합니다');
+      return;
+    }
+    setIsAnonymous((prev) => !prev);
+  }, [authUser]);
 
   return (
     <ServiceLayout title="user home">
@@ -27,7 +51,11 @@ const UserHomePage: NextPage = function () {
 
       <Box mt={4} bgColor="white">
         <Flex p={2}>
-          <Avatar src="https://bit.ly/broken-link" size="xs" />
+          <Avatar
+            src={isAnonymous ? ANONYMOUS_IMAGE_URL : authUser?.photoURL ?? ANONYMOUS_IMAGE_URL}
+            size="sm"
+            padding={1}
+          />
           <Textarea
             bg="gray.100"
             border="none"
@@ -40,21 +68,26 @@ const UserHomePage: NextPage = function () {
             maxRows={7}
             as={ResizeTextArea}
             value={message}
-            onChange={(e) => {
-              const { value } = e.currentTarget;
-              const lineCount = value.match(/[^\n]*\n[^\n]*/gi)?.length ?? 0;
-              if (lineCount >= 7) {
-                return;
-              }
-
-              setMessage(value);
-            }}
+            onChange={handleChangeTextarea}
           />
 
           <Button disabled={!message} bgColor="#ffb86c" color="white" colorScheme="yellow" size="sm">
             등록
           </Button>
         </Flex>
+        <FormControl display="flex" alignItems="center" m={2}>
+          <Switch
+            size="sm"
+            colorScheme="orange"
+            id={ANONYMOUS_SWITCH_ID}
+            mr="1"
+            isChecked={isAnonymous}
+            onChange={handleChangeSwitch}
+          />
+          <FormLabel htmlFor={ANONYMOUS_SWITCH_ID} mb="0" fontSize="xx-small">
+            Anonymous
+          </FormLabel>
+        </FormControl>
       </Box>
     </ServiceLayout>
   );
